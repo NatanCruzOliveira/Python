5723c894cd14c478e63525b3a73106115eadc5c5
Natã Oliveira <nataoliveira190497@gmail.com>
2025-03-03 17:16:41 -0300

    update desafio1

--- desafio1/desafio1.py
+++ desafio1/desafio1.py
@@ -2,4 +2,5 @@ nome = input('Qual é seu nome?')
 sobrenome = input('Qual é seu sobrenome?')
 idade = input('Qual é sua idade?')
 
-print('Bem vindo,seu nome é', nome, sobrenome,'e você tem', idade, 'anos')
+print('Bem vindo, seu nome é', nome, sobrenome,'e você tem', idade, 'anos')
+
