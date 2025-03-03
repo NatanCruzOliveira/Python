c53256ae5009ba6ca9cf356c222b989936a068e7
Natã Oliveira <nataoliveira190497@gmail.com>
2025-03-03 16:51:04 -0300

    create desafio 2 e desafio 3

---
+++ desafio2/desafio2.py
@@ -0,0 +1,5 @@
+dia = input('Digite dia nascimento: ')
+mes = input('Digite mês de nascimento: ')
+ano = input('Digite ano de nascimento: ')
+
+print('você nasceu no dia', dia, 'de', mes, 'em', ano)
