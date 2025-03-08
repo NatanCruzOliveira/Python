61d2041a1d88010e4fe03b3a5151f0b8a292b025
Natã Oliveira <nataoliveira190497@gmail.com>
2025-03-08 13:48:30 -0300

    Create desafio7.py

---
+++ Guanabara/desafio7/desafio7.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+media = (n1 + n2) / 2
+
+print('Analisando o número digitado {} e {}, sua média é {}'.format(n1, n2, media))
